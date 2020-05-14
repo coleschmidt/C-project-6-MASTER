43549dab8a9ae0dc0e126292feeefeb07c38a30f
Mr_Keyboard <colecschmidt@gmail.com>
2020-05-14 10:02:02 -0500

    testbed code for later improvement
    new code from testbed backup

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char const *argv[])
+{
+	int array[100000], results[100000]; 
+	int search, num, num_r, count, div_count = 0;
 
-int main(int argc, char *argv[]) {
+	printf("How many numbers do you want?\n");
+	scanf("%d", &count);
+
+	printf("Enter %d intergers(s)\n", count);
+
+	for (num = 0; num < count; num++){
+			scanf("\n %d", &array[num]);
+		}
+
+	for (num = 0; num < count; num++){
+		if ((array[num] % 7) == 0){
+			results[div_count] = array[num];
+			div_count++;
+			printf("There is a divisble in your array\n");
+
+		}
+	}
+
+	printf("the numbers in the array are: \n");
+
+	for (num = 0; num < count; num++){
+		printf(" %d", array[num]);
+	}
+
+	printf("the divisble numbers are: \n");
+
+	for (num = 0; num < div_count; num++){
+		printf(" %d\n", results[num]);
+	}
+
 	return 0;
 }
